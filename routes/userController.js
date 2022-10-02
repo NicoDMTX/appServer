dee3e2e8bf5e1ed6b8cc9e385adb1d56dff1dff6
NicoDMTX <nicolas.demontoux@hotmail.com>
2022-10-02 23:10:24 +0200

    🚧 Still get an error but close to find

--- routes/userController.js
+++ routes/userController.js
@@ -24,28 +24,37 @@ class User {
 
     RegisterUser(item, url) {
         router.post(url, async (req, res) => {
-            const newUser = new item({
-                firstName: req.body.firstName,
-                password: req.body.password,
-            })
 
-            const salt = await bcrypt.genSalt(10)
-            newUser.password = await bcrypt.hash(newUser.password, salt)
-
-            // check joi conditions
-            const userRules = userValidation(newUser);
-            console.log(userRules)
-
-            if (userRules) {
-                newUser.save((err, docs) => {
-                    if (!err) {
-                        return res.send(docs);
-                    }
-                    return console.log('Error creating data :' + err)
+            try {
+                const newUser = new item({
+                    firstName: req.body.firstName,
+                    password: req.body.password,
                 })
-            }  else {
-                return res.status(401).json(userRules.error.details[0]);
+
+                const userRules = userValidation(newUser);
+                if (!userRules.password) { 
+                    const salt = await bcrypt.genSalt(10)
+                    
+                    newUser.password = await bcrypt.hash(newUser.password, salt)
+                } else {
+                    return res.status(401)
+                }
+
+                if (userRules.firstName) {
+                    await newUser.save((err, docs) => {
+                        if (!err) {
+                            return res.send(docs);
+                        }
+                        return console.log('Error creating data :' + err)
+                    })
+                } else {
+                    return res.status(401).json(userRules.error.message);
+                }
+                
+            } catch(e) {
+                return console.log(e)
             }
+
         })
     }
 }
--- validation/validation.js
+++ validation/validation.js
@@ -2,10 +2,11 @@ const joi = require('joi');
 
 const userValidation = (body) => {
     const userValidationSchema = joi.object({
-        firstName: joi.string().min(4).max(12).trim().required()
+        firstName: joi.string().max(12).min(4).trim().required(),
+        password: joi.string().max(16).min(4).trim().required()
     })
 
-    return userValidationSchema.validate(body)
+    return userValidationSchema.validate(body._doc)
 }
 
 module.exports = { userValidation }
